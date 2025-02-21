cafc34d75713746f1a4618327d29e306cb82e8c1
Huỳnh Thanh Phương <119808395+phuong261104@users.noreply.github.com>
2025-02-21 16:05:34 +0700

    Bài 4 minh chứng

--- lab_tuan2/src/App.jsx
+++ lab_tuan2/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <Ex3 />
+      <Ex4 />
 
     </>
   )
--- lab_tuan2/src/component/ex4.jsx
+++ lab_tuan2/src/component/ex4.jsx
@@ -5,33 +5,60 @@ const Ex4 = () => {
     const [name, setName] = useState("");
     const [todoData, setTodoData] = useState([])
 
-    const addToDo = (name) => {
-        setTodoData([...todoData, name])
-    }
+    const addToDo = () => {
+        if (name.trim() === "") {
+            alert("Vui lòng nhập công việc!");
+            return;
+        }
+        setTodoData([...todoData, name]);
+        setName("");
+    };
+
+    const deleteToDo = (index) => {
+        const newList = todoData.filter((_, i) => i !== index);
+        setTodoData(newList);
+    };
 
 
     return (
 
         <>
-        <h3>Ex4</h3>
-        <div>
-        <input type="text" style={{marginRight: "10px"}} onChange={(e)=>{setName(e.target.value)}} placeholder="input name"/>
-        <span style={{marginRight: "10px"}}>This is input: {name}</span>
-        <button onClick={()=>addToDo(name)} >Them</button>
-        </div>
-        {todoData
-        ? <div>{
-            todoData.map((item, index) => {
-                return (
-                    <div style={{margin: "10px"}}>
-                        {item}
-                        <button>delete</button>
-                    </div>
-                )
-            })
-        } </div>:
-        <span>No data</span>
-        }
+            <h1>To do list</h1>
+            <div>
+                <input
+                    value={name}
+                    style={{ margin: "10px", width: "25vw", fontSize: "20px", borderRadius: "10px", padding: "10px" }}
+                    type="text" onChange={(e) => { setName(e.target.value) }} placeholder="input name" />
+                <button
+                    style={{ padding: "10px", border: "1px solid #ccc" }}
+                    onClick={() => addToDo(name)} >Them</button>
+
+                {todoData
+                    ? <div style={{ width: "30vw", padding: "10px" }} >
+                        {todoData.map((item, index) => (
+                            <div key={index} style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center"
+                            }}>
+                                {item}
+                                <button
+                                    style={{
+                                        margin: "10px",
+                                        fontSize: "20px",
+                                        borderRadius: "10px",
+                                        padding: "10px",
+                                        color: "white", backgroundColor: "red"
+                                    }}
+                                    onClick={() => deleteToDo(index)} >
+                                    Xóa
+                                </button>
+                            </div>
+                        ))}
+                    </div> :
+                    <span>No data</span>
+                }
+            </div>
         </>
     )
 }

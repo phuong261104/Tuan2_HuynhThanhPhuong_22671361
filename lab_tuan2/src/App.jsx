db52cd26358b3a0f83540fd68c66d48d37134598
Huỳnh Thanh Phương <119808395+phuong261104@users.noreply.github.com>
2025-02-21 15:20:54 +0700

    Bài 2 minh chứng

--- lab_tuan2/src/App.jsx
+++ lab_tuan2/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <Ex1 />
+      <Ex2 />
 
     </>
   )
--- lab_tuan2/src/component/ex2.jsx
+++ lab_tuan2/src/component/ex2.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react"
 
 const Ex2 = () => {
-    const [a, setA] = useState(0)
-    const [b, setB] = useState(0)
-    const [rs, setRs] = useState()
+    const [a, setA] = useState("")
+    const [b, setB] = useState("")
+    const [rs, setRs] = useState(null)
+
+    const numA = Number(a)
+    const numB = Number(b)
 
     const handleOnClick = () => {
-        if(a !== null && b !== null){
-            console.log(a,b)
-            setRs(Number(a)+Number(b))
+        if (a === "" || b === "") {
+            console.log(a, b)
+            alert("Vui long nhap so hop le")
         }
+        setRs(a + b)
     }
 
     return (
         <>
-        <h3>Ex2: sum</h3>
-        <input type="number" onChange={(e)=>{setA(e.target.value)}} placeholder="So a" style={{marginRight: "10px"}}/>
-        <input type="number" onChange={(e)=>{setB(e.target.value)}}placeholder="So b" style={{marginRight: "10px"}}/>
-        <button style={{marginRight: "10px"}} onClick={() => {handleOnClick()}}>Sum</button>
-        {
-            rs?<span>{rs}</span>: <span>Vui long nhap so</span>
-        }
+            <h1>Ex2: SUM</h1>
+            <div>
+                <input
+                    style={{ margin: "10px", width: "60vw", fontSize: "20px", borderRadius: "10px", padding: "10px" }}
+                    type="number" onChange={(e) => { setA(e.target.value) }} placeholder="So a" />
+                <input
+                    style={{ margin: "10px", width: "60vw", fontSize: "20px", borderRadius: "10px", padding: "10px" }}
+                    type="number" onChange={(e) => { setB(e.target.value) }} placeholder="So b" />
+            </div>
+            <button style={{ marginRight: "10px", padding: "10px" }} onClick={() => { handleOnClick() }}>Sum</button>
+            <hr />
+            {
+                rs ? <span>{rs}</span> : <span>Vui long nhap so</span>
+            }
         </>
     )
 }
